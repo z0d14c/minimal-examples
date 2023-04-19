e0874daf9f9c1f96d974fd7bdadc06261c017808
LinuxMint <linuxmint@linuxmint203.linuxvmimages.local>
2023-04-19 18:18:03 -0400

    fix: tweak notes in wasm module

--- src/pages/WebAssembly/main.go
+++ src/pages/WebAssembly/main.go
@@ -1,5 +1,8 @@
 // note: this must be compiled as follows
 // GOOS=js GOARCH=wasm go build -o main.wasm main.go
+// ultimately the WebAssembly/ folder should have `main.go` and `wasm_exec.js`
+// `wasm_exec.js` comes from the Go installation, and was located at
+// /usr/local/go/misc/wasm/wasm_exec.js
 package main
 
 import (
@@ -14,6 +17,8 @@ func consoleLog(args ...interface{}) {
 
 func multiply(this js.Value, inputs []js.Value) interface{} {
 	consoleLog("Multiply inputs", inputs[0], inputs[1])
+	// the below code didn't work for me, but leaving for reference
+	// I suspect it is because the inputs are already numbers when they're passed in
 	// a, errA := strconv.Atoi(inputs[0].String())
 	// if errA != nil {
 	// 	consoleLog("Error converting first input to integer:")
